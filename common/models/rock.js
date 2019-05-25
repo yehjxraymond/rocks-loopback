d28d426c0494b9f8a8d9a372b2b6b21178765256
Raymond Yeh <yehjxraymond@gmail.com>
2019-05-25 22:08:41 +0800

    Simple loopback

---
+++ common/models/rock.js
@@ -0,0 +1,28 @@
+"use strict";
+const axios = require("axios");
+
+const getJoke = async () => {
+  const randomJoke = await axios({
+    method: "get",
+    headers: {
+      Accept: "application/json"
+    },
+    url: "https://icanhazdadjoke.com/"
+  });
+  return randomJoke.data.joke;
+};
+
+module.exports = function(Rock) {
+  Rock.observe("before save", async (ctx, next) => {
+    if (ctx.instance) {
+      if (!ctx.instance.engraving) {
+        ctx.instance.engraving = await getJoke();
+      }
+    } else {
+      if (ctx.data.engraving) {
+        ctx.data.engraving = await getJoke();
+      }
+    }
+    next();
+  });
+};
